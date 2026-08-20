f57e72376ff242b5f341ae0d4166bf26b4e9ef26
Maxim <besstrakhov.m@gmail.com>
2026-08-20 22:57:52 +0300

    add Carousel

--- components/widgets/weatherForHours.tsx
+++ components/widgets/weatherForHours.tsx
@@ -3,6 +3,7 @@ import {dayDataType, sunDataType} from "@/costants/types";
 import {JSX} from "react";
 import {Card, CardFooter, CardHeader} from "@/components/ui/card";
 import ConditionalImage from "@/components/widgets/features/conditionalImage";
+import {Sunrise, Sunset} from "lucide-react";
 
 interface Props {
     data: dayDataType[]
@@ -16,6 +17,7 @@ export default function WeatherForHours({data, sun}: Props): JSX.Element {
     const sunRise = sun.sunrise.split(":")[0]
     const sunSet = sun.sunset.split(":")[0]
 
+
     return (
         <div className={"mt-8"}>
             <Carousel
@@ -25,17 +27,45 @@ export default function WeatherForHours({data, sun}: Props): JSX.Element {
                 className=""
             >
                 <CarouselContent>
-                    {data.map((item, i) => (
-                        <CarouselItem key={i + 1} className={"basis-1/5"}>
-                            <Card
-                                className={"text-white bg-transparent ring-0 shadow-0 border-0 flex-col items-center justify-center p-0 m-0 "}>
-                                <CardHeader className={"text-[13px] text-center w-full mb-2"}>{item.time}</CardHeader>
-                                <ConditionalImage status={""+item.condition.code} isDay={item.time > sunRise && item.time < sunSet}/>
+                    {data.reduce<JSX.Element[]>((acc, item, i) => {
+
+                        acc.push(
+                            <CarouselItem key={i + 1} className={"basis-1/5"}>
+                                <Card
+                                    className={"text-white bg-transparent ring-0 shadow-0 border-0 flex-col items-center justify-center p-0 m-0 "}>
+                                    <CardHeader
+                                        className={"text-[13px] text-center w-full mb-2"}>{item.time}</CardHeader>
+                                    <ConditionalImage status={"" + item.condition.code}
+                                                      isDay={item.time > sunRise && item.time < sunSet}/>
+                                    <CardFooter className={"text-[17px]"}>{item.temperature}</CardFooter>
+                                </Card>
+                            </CarouselItem>
+                        )
+
+                        if (item.time === sunRise || item.time === sunSet) {
+                            const isSunRise = item.time === sunRise
+                            acc.push(
+                                <CarouselItem key={(i + 1) * 10} className={"basis-1/5"}>
+                                    <Card
+                                        className={"text-white bg-transparent ring-0 shadow-0 border-0 flex-col items-center justify-center p-0 m-0 "}>
+                                        <CardHeader className={"text-[13px] text-center w-full mb-2"}>{
+                                            isSunRise ? sun.sunrise : sun.sunset}</CardHeader>
+                                        {isSunRise ?
+                                            <Sunrise width={28} height={28} color={"#8cdf49"}/> :
+                                            <Sunset width={28} height={28} color={"#ffffff"}/>
+                                        }
+                                        <CardFooter className={"text-[15px]"}>{isSunRise?"SUNRISE":"SUNSET"}</CardFooter>
+                                    </Card>
+                                </CarouselItem>
+                            )
 
 
-                                <CardFooter className={"text-[17px]"}>{item.temperature}</CardFooter>
-                            </Card>
-                        </CarouselItem>))}
+                        }
+
+                        return acc
+                    }, [])
+
+                    }
                 </CarouselContent>
                 {/*<CarouselPrevious />*/}
                 {/*<CarouselNext />*/}
