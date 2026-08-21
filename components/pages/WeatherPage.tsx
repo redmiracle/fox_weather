7562beb4573b3ed44d85de5cee9932b879e030c3
Maxim <besstrakhov.m@gmail.com>
2026-08-21 17:04:48 +0300

    add Carousel

--- components/pages/WeatherPage.tsx
+++ components/pages/WeatherPage.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-// import {useQuery} from "@tanstack/react-query";
-// import {getWeatherData} from "@/lib/api/getWeather";
-// import {useEffect} from "react";
-// import {Card, CardHeader} from "@/components/ui/card";
+import {useQuery} from "@tanstack/react-query";
+import {getWeatherData} from "@/lib/api/getWeather";
+import {useEffect} from "react";
+
 import {dataJson} from "@/costants/dataJson";
 import CityWeather from "@/components/widgets/cityWeather";
 import WeatherForHours from "@/components/widgets/weatherForHours";
@@ -12,36 +12,35 @@ import WeatherForHours from "@/components/widgets/weatherForHours";
 export default function WeatherPage() {
 
 
-    // const {data,isLoading,error}=useQuery({
-    //     queryKey:['weather'],
-    //     queryFn:getWeatherData
-    // })
-    //
-    //
+    const {data,isLoading,error}=useQuery({
+        queryKey:['weather'],
+        queryFn:getWeatherData
+    })
+
+
     // useEffect(() => {
     //     if(data) {;
     //         console.log(data);
     //     }
     // }, [data])
-    //
-    // if (isLoading) {
-    //     return <h1>Loading...</h1>;
-    // }
+
+    if (isLoading) {
+        return <h1>Loading...</h1>;
+    }
 
 
-    // if (error) {
-    //     return <h1>Error: {error.message}</h1>;
-    //
-    // }
+    if (error) {
+        return <h1>Error: {error.message}</h1>;
+
+    }
 
 
     return (<div>
 
-        {/*{data && data.preview.current.name}*/}
-        {dataJson &&
+        {data &&
             <div className={"px-2"}>
-                <CityWeather data={dataJson.preview.current} />
-                <WeatherForHours data={ dataJson.preview.forecast} sun={dataJson.preview.week.temperature.days[0].sun} />
+                <CityWeather data={data.preview.current} />
+                <WeatherForHours data={ data.preview.forecast} sun={data.preview.week.temperature.days[0].sun} />
 
             </div>
 
